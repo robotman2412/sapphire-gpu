efe0070581eb1623a73f5e651ec62cbd3da6165e
RobotMan2412 <julian@scheffers.net>
2024-02-23 00:35:59 +0100

    Incrementing stub

--- hdl/rasterizer/saph_float_incrementer.sv
+++ hdl/rasterizer/saph_float_incrementer.sv
@@ -9,120 +9,43 @@
 // Multi-float incrementing unit.
 module saph_float_incrementer#(
     // Number of floats to compute.
-    parameter   integer numbers = 2,
-    // Number of floating-point adders to use.
-    parameter   integer adders  = 1,
-    // Number of cycles required for addition.
-    localparam  integer delay   = numbers / adders
+    parameter   integer numbers = 2
 )(
     // Core clock.
-    input  logic    clk,
+    input  logic                clk,
     // Synchronous reset.
-    input  logic    rst,
+    input  logic                rst,
+    // Floating-point unit interfaces.
+    saph_fpi.GPU                fpi[numbers],
     
     // Latch init values.
-    input  logic    latch,
+    input  logic                latch,
     // Enable increment.
     // Overriden by latch.
-    input  logic    count,
+    input  logic[numbers-1:0]   count,
     // Incremented value is ready after posedge clk.
-    output logic    ready,
+    output logic                ready,
     
     // Initial state.
-    input  float    init[numbers],
+    input  float                init[numbers],
     // Increment values.
-    input  float    inc[numbers],
+    input  float                inc[numbers],
     // Current state.
-    output float    cur[numbers]
+    output float                cur[numbers]
 );
     genvar x;
+    localparam integer latency = fpi.latency;
     
-    // Increment buffer.
-    float inc_reg[numbers];
-    // Addition result.
-    float res[adders];
+    // Additions in progress.
+    logic[numbers-1:0]  busy;
     
+    // Floating-point interface logic.
     generate
-        // For multicycle: Currently busy counting.
-        logic counting;
-        // For multicycle: Current bank being counted on.
-        logic[$clog2(delay)-1:0] bank;
-        
-        // Increment latching logic.
         for (x = 0; x < numbers; x = x + 1) begin
-            always @(posedge clk) begin
-                if (latch) begin
-                    inc_reg[x] <= inc[x];
-                end
-            end
-        end
-        
-        if (delay > 1) begin: d1
-            // Counting multicycle.
-            assign  ready   = counting && bank == delay - 1;
-            
-            // Counting state machine.
-            always @(posedge clk) begin
-                if (rst || latch) begin
-                    // Reset.
-                    counting    <= 0;
-                    bank        <= 0;
-                end else if (counting && bank == delay - 1) begin
-                    // Last cycle of counting.
-                    counting    <= 0;
-                    bank        <= 0;
-                end else if (counting) begin
-                    // Busy counting.
-                    bank        <= bank + 1;
-                end else if (count) begin
-                    // Start counting.
-                    counting    <= 1;
-                    bank        <= bank + 1;
-                end
-            end
-            
-            // Latching logic.
-            for (x = 0; x < numbers; x = x + 1) begin
-                always @(posedge clk) begin
-                    if (rst) begin
-                        // Reset.
-                        cur[x] <= 'bx;
-                    end else if (latch) begin
-                        // Latch initial state.
-                        cur[x] <= init[x];
-                    end else if (count && bank == 0 && x / adders == 0) begin
-                        // Count (first bank).
-                        cur[x] <= res[x % adders];
-                    end else if (counting && bank == x / adders && x / adders >= 1) begin
-                        // Count (second and later banks).
-                        cur[x] <= res[x % adders];
-                    end
-                end
-            end
-            
-        end else begin: d2
-            // Adding all at once.
-            assign  bank        = 0;
-            assign  counting    = 0;
-            assign  ready       = 1;
-            
-            // Latching logic.
-            for (x = 0; x < adders; x = x + 1) begin
-                always @(posedge clk) begin
-                    if (rst) begin
-                        // Reset.
-                        cur[x] <= 'bx;
-                    end else if (count) begin
-                        // Count.
-                        cur[x] <= res[x];
-                    end
-                end
-            end
-        end
-        
-        // Adder array.
-        for (x = 0; x < adders; x = x + 1) begin
-            svfloat_add#(float) fadd(clk, inc_reg[x+bank*adders], cur[x+bank*adders], res[x]);
+            logic r_busy;
+            assign fpi[x].d_trig = trig || r_busy;
+            assign fpi[x].lhs    = cur[x];
         end
     endgenerate
+    
 endmodule
