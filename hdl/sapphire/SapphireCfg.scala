a2fe5c03874454c03ae3aecd5c7157f9873bf792
RobotMan2412 <julian@scheffers.net>
2024-09-25 19:10:01 +0200

    Texture reader

--- hdl/sapphire/SapphireCfg.scala
+++ hdl/sapphire/SapphireCfg.scala
@@ -6,7 +6,20 @@ import spinal.core._
 
 case class SapphireCfg(
     /** Pixel coordinate bit width. */
-    pixelBits: Int = 12,
+    pixelBits: Int           = 12,
     /** Virtual address bit width. */
-    vaddrBits: Int = 32
+    vaddrBits: Int           = 32,
+    /** Pipeline topology configuration. */
+    plCfg:     SapphirePlCfg = SapphirePlCfg()
+)
+
+case class SapphirePlCfg(
+    /** Use a stage for f2i / i2f. */
+    fconvStage:     Boolean = true,
+    /** Normal multiply stage count. */
+    normMulStages:  Int     = 3,
+    /** Small (8-bit) multiply stage count. */
+    smallMulStages: Int     = 2,
+    /** Use a stage for address calculation. */
+    acalcStage:     Boolean = true
 )
--- hdl/sapphire/texture/TextureReader.scala
+++ hdl/sapphire/texture/TextureReader.scala
@@ -5,15 +5,106 @@ package sapphire.texture
 import spinal.core._
 import sapphire._
 import spinal.lib.experimental.math._
+import spinal.lib._
+import spinal.lib.misc.pipeline._
+import spinal.lib.bus.amba3.ahblite._
 
 /** Accepts floating-point UV coordinates and reads raw texture data. */
 case class TextureReader(cfg: SapphireCfg) extends Component {
+    import cfg.plCfg
     val io = new Bundle {
-        /** 0-1 texture X coordinate. */
-        val u       = in port Floating32()
-        /** 0-1 texture Y coordinate. */
-        val v       = in port Floating32()
+        /** Texture coordinate input stream. */
+        val uv      = slave  port Stream(Vec.fill(2)(Floating32()))
+        /** Texture data output stream. */
+        val data    = master port Stream(Bits(32 bits))
         /** Texture reference. */
-        val texture = in port TextureRef(cfg)
+        val texture = in     port TextureRef(cfg)
+        /** Memory interface. */
+        val mem     = master port AhbLite3Master(AhbLite3Config(cfg.vaddrBits, 32))
+    }
+    
+    val nodes = Array.fill(
+        plCfg.fconvStage.toInt + 2*plCfg.normMulStages + plCfg.acalcStage.toInt + 1
+    )(Node())
+    val f2iNode   = nodes(0)
+    val mulNode   = nodes(plCfg.fconvStage.toInt)
+    val icalcNode = nodes(plCfg.fconvStage.toInt + plCfg.normMulStages)
+    val acalcNode = nodes(plCfg.fconvStage.toInt + 2*plCfg.normMulStages)
+    val memNode0  = acalcNode
+    val memNode1  = nodes(plCfg.fconvStage.toInt + 2*plCfg.normMulStages + plCfg.acalcStage.toInt)
+    
+    /** Convert UVs to [0,1) fixed-point numbers. */
+    val f2i = new f2iNode.Area {
+        arbitrateFrom(io.uv)
+        val UF = insert(io.uv.payload(0))
+        val VF = insert(io.uv.payload(1))
+        val UI = insert(UF.toRecFloating.toUFix(0 exp, 24 bits))
+        val VI = insert(VF.toRecFloating.toUFix(0 exp, 24 bits))
+    }
+    
+    /** Multiply quantized UVs into X and Y coordinates. */
+    val mul = new mulNode.Area {
+        val range = cfg.pixelBits + 23 downto 24
+        val X = insert((io.texture.spec.width  * f2i.UI.raw)(range))
+        val Y = insert((io.texture.spec.height * f2i.VI.raw)(range))
+    }
+    
+    /** Compute texture index of pixel. */
+    val icalc = new icalcNode.Area {
+        val IDX = insert(mul.X + mul.Y * io.texture.spec.width)
+    }
+    
+    /** Compute bit and byte address of pixel. */
+    val acalc = new acalcNode.Area {
+        val OFF  = insert(io.texture.spec.pixfmt.bpp.mux(
+            M"00000-" -> (icalc.IDX >> 3)            .resize(26),
+            M"00001-" -> (icalc.IDX >> 2)            .resize(26),
+            M"0001--" -> (icalc.IDX >> 1)            .resize(26),
+            M"001---" ->  icalc.IDX                  .resize(26),
+            M"010---" -> (icalc.IDX << 1)            .resize(26),
+            M"011---" -> (icalc.IDX + icalc.IDX << 1).resize(26),
+            M"1-----" -> (icalc.IDX << 2)            .resize(26),
+        ))
+        val SHR  = insert(io.texture.spec.pixfmt.bpp.mux(
+            M"00000-" -> icalc.IDX(2 downto 0),
+            M"00001-" -> icalc.IDX(1 downto 0).resize(3),
+            M"0001--" -> icalc.IDX(0 downto 0).resize(3),
+            default   -> U"000",
+        ))
+        val ADDR = insert(io.texture.vaddr + OFF.resize(cfg.vaddrBits))
+    }
+    
+    /** Memory request logic. */
+    val mem0 = new memNode0.Area {
+        // Set constant memory bus signals.
+        io.mem.HWDATA.assignDontCare()
+        io.mem.HWRITE    := False
+        io.mem.HSIZE     := B"010"
+        io.mem.HPROT     := B"1111" // Cacheable bufferable privileged data
+        io.mem.HMASTLOCK := False
+        io.mem.HBURST    := B"000"
+        
+        // Set dynamic memory bus signals.
+        io.mem.HADDR  := acalc.ADDR
+        io.mem.HADDR(1 downto 0) := U"00"
+        io.mem.HTRANS := AhbLite3.IDLE
+        when (isValid) {
+            io.mem.HTRANS := AhbLite3.NONSEQ
+        }
+        
+        // Block if the memory is not ready.
+        when (!io.mem.HREADY) {
+            ready := False
+        }
+    }
+    
+    /** Memory response logic. */
+    val mem1 = new memNode1.Area {
+        val RDATA = insert(io.mem.HRDATA)
+        io.data.payload(8 downto 0) := RDATA(8 downto 0) >> acalc.SHR
+        when (!io.mem.HREADY) {
+            valid := False
+        }
+        arbitrateTo(io.data)
     }
 }
